67944a5c1ac698f2d2b3c8a8804e4e9a8f6858f3
YaGrig <yashagrigorev@gmail.com>
2021-05-23 20:12:03 +0300

    fix & update: add body parser to app.js and update pg version to 8.0.3

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
+const express = require("express"); 
 var app = express();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
+const bp = require("body-parser");
 
 
 db.sync();
-app.use(require('body-parser'));
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
